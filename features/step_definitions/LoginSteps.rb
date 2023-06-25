440b9cc51d99e3256b884a3d8d50c41b030aa94f
mikhshal <110817678+mikhshal@users.noreply.github.com>
2023-06-25 17:49:58 +0500

    Another small update

--- features/step_definitions/LoginSteps.rb
+++ features/step_definitions/LoginSteps.rb
@@ -9,10 +9,7 @@ end
 Then("User inputs the Email and Password") do
   @driver.find_element(:xpath, '//input[@placeholder = "mail@example.com"]').send_keys 't.e.s.t.mihannod99@gmail.com'
   @driver.find_element(:xpath,'//input[@placeholder = "password"]').send_keys 'hGh^198O]QYd'
-  @driver.find_element(:xpath, '//button[@class="button big primary" and text()="Login"]').click
-  sleep(2)
 end
-
 Then("User press the Login button") do
   @driver.find_element(:xpath, '//button [@class = "button big primary"]').click
 end
